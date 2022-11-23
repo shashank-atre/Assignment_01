ff589cb5b4b5dc03082b9425d6fe76285efb5bd6
shashank-atre <117571654+shashank-atre@users.noreply.github.com>
2022-11-23 18:46:18 +0530

    Update 01_Assignment.py

--- 01_Assignment.py
+++ 01_Assignment.py
@@ -268,4 +268,15 @@ else:
 
 print("The largest number is", largest)
 
+#Q25. Write a program to display only those numbers from a list that satisfy the following conditions
+
+# The number must be divisible by five
+
+# If the number is greater than 150, then skip it and move to the next number
+
+# If the number is greater than 500, then stop the loop
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+# Answer--> 
+multiple =print(list(filter(lambda numbers:numbers%5==0,[12, 75, 150, 180, 145, 525, 50])))
 
